8b19c3e70b41ae6d342897bd974bb9c3c3d61aae
minhminh090 <114343317+minhminh090@users.noreply.github.com>
2022-09-26 13:20:09 +0700

    thu code

---
+++ bai1_TH.cpp
@@ -0,0 +1,19 @@
+//Pham Minh Nhut_MSSV:6251020075_KTDTVTK62
+#include<stdio.h>
+#include<math.h>
+int main(){
+	int a , b;  
+	printf("hay nhap vao a: ");
+	scanf("%d", &a);
+	printf("hay nhap vao b: ");
+	scanf("%d", &b);
+	float C,S;
+	C= (a+b)*2;
+	S= a*b;
+	printf("\nchu vi hinh chu nhat=%f",C);
+	printf("\ndien tich hinh chu nhat=%f",S);
+    if (a < 0, b < 0)
+	   printf("khong tinh duoc, vui long nhap lai");
+ 	   return 0;
+}
+ 
